e88cf7005354419ee6592d0b47bfb40e9ce6dbd4
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-03-12 11:19:40 -0700

    Initial Commit

---
+++ TinderClone/TinderCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderCloneApp.swift
+//  TinderClone
+//
+//  Created by özgün aksoy on 2024-03-12.
+//
+
+import SwiftUI
+
+@main
+struct TinderCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
