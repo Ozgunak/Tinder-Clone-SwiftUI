17989a354ad1f7e6e93b7fd65f80b68f07c61ff6
Ozgun Aksoy <zgnaksoy@gmail.com>
2024-03-15 19:19:50 -0700

    add button overlay

---
+++ TinderClone/Core/Cards/Views/CardImageButtonOverlay.swift
@@ -0,0 +1,31 @@
+//
+//  CardImageButtonOverlay.swift
+//  TinderClone
+//
+//  Created by özgün aksoy on 2024-03-15.
+//
+
+import SwiftUI
+
+struct CardImageButtonOverlay: View {
+    @Environment(CardViewModel.self) private var cardVM
+    var body: some View {
+        HStack {
+            Rectangle()
+                .onTapGesture {
+                    cardVM.updateIndex(isIncrement: false)
+                }
+            
+            Rectangle()
+                .onTapGesture {
+                    cardVM.updateIndex(isIncrement: true)
+                }
+        }
+        .foregroundStyle(.white.opacity(0.001))
+    }
+}
+
+#Preview {
+    CardImageButtonOverlay()
+        .environment(CardViewModel())
+}
